a955b18ae5a987919b467557efefaf5583d77c32
acdevine89 <annie.devine@detroitlabs.com>
2014-11-16 18:27:56 -0500

    Refactoring drawables confeti2 and confeti3 in animated_confetti.xml to be confetti2 and confetti3 (fixing typo)

--- LeonidasExamples/src/com/plattysoft/leonids/examples/ConfettiExampleActivity.java
+++ LeonidasExamples/src/com/plattysoft/leonids/examples/ConfettiExampleActivity.java
@@ -18,13 +18,13 @@ public class ConfettiExampleActivity extends Activity implements OnClickListener
 
 	@Override
 	public void onClick(View arg0) {		
-		new ParticleSystem(this, 80, R.drawable.confeti2, 10000)
+		new ParticleSystem(this, 80, R.drawable.confetti2, 10000)
 		.setSpeedModuleAndAngleRange(0f, 0.1f, 180, 180)
 		.setRotationSpeed(144)
 		.setAcceleration(0.000017f, 90)		
 		.emit(findViewById(R.id.emiter_top_right), 8);
 				
-		new ParticleSystem(this, 80, R.drawable.confeti3, 10000)
+		new ParticleSystem(this, 80, R.drawable.confetti3, 10000)
 		.setSpeedModuleAndAngleRange(0f, 0.1f, 0, 0)
 		.setRotationSpeed(144)
 		.setAcceleration(0.000017f, 90)		

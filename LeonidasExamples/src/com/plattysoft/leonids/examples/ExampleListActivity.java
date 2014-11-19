d363df97ade4774ad6aece835020555e1375092e
plattysoft <raul@plattysoft.com>
2014-11-19 11:28:52 +0000

    added stub of new example with gravity

--- LeonidasExamples/src/com/plattysoft/leonids/examples/ExampleListActivity.java
+++ LeonidasExamples/src/com/plattysoft/leonids/examples/ExampleListActivity.java
@@ -14,12 +14,13 @@ public class ExampleListActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		String sampleList[] = new String [] {"One Shot Simple", "One Shot Advanced", 
 				"Emiter Simple", "Emiter Intermediate", "Emiter Time Limited"
+				, "Emit with Gravity"
+				, "Follow cursor"
 				, "Animated particles"
 				, "Fireworks"
 				, "Confetti [Rabbit and Eggs]"
 				, "Dust [Rabbit and Eggs]"
 				, "Stars [Rabbit and Eggs]"
-				, "Follow cursor"
 //				, "Animated Particles"
 				};
 		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sampleList));
@@ -41,26 +42,29 @@ public class ExampleListActivity extends ListActivity {
 			startActivity(new Intent(this, EmiterIntermediateExampleActivity.class));
 			break;
 		case 4:
-			startActivity(new Intent(this, EmiterTimeLimitedExampleActivity.class));
+			startActivity(new Intent(this, EmiterWithGravityExampleActivity.class));
 			break;
 		case 5:
-			startActivity(new Intent(this, AnimatedParticlesExampleActivity.class));
-			break;
-		case 6:
-			startActivity(new Intent(this, FireworksExampleActivity.class));
-			break;
-		case 7:
-			startActivity(new Intent(this, ConfettiExampleActivity.class));
-			break;
-		case 8:
-			startActivity(new Intent(this, DustExampleActivity.class));
-			break;
-		case 9:
-			startActivity(new Intent(this, StarsExampleActivity.class));
-			break;
-		case 10:
 			startActivity(new Intent(this, FollowCursorExampleActivity.class));
 			break;
+		case 6:
+			startActivity(new Intent(this, EmiterTimeLimitedExampleActivity.class));
+			break;
+		case 7:
+			startActivity(new Intent(this, AnimatedParticlesExampleActivity.class));
+			break;
+		case 8:
+			startActivity(new Intent(this, FireworksExampleActivity.class));
+			break;
+		case 9:
+			startActivity(new Intent(this, ConfettiExampleActivity.class));
+			break;
+		case 10:
+			startActivity(new Intent(this, DustExampleActivity.class));
+			break;
+		case 11:
+			startActivity(new Intent(this, StarsExampleActivity.class));
+			break;		
 		}
 	}
 

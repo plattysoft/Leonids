c4f9a66e56e1866b9bf3e7fb6242f10fcf138c15
erkan <erkan@peakgames.net>
2015-10-05 13:15:41 +0300

    example fix

--- LeonidsExamples/src/main/java/com/plattysoft/leonids/examples/ConfettiExampleActivity.java
+++ LeonidsExamples/src/main/java/com/plattysoft/leonids/examples/ConfettiExampleActivity.java
@@ -1,11 +1,10 @@
 package com.plattysoft.leonids.examples;
 
-import com.plattysoft.leonids.ParticleSystem;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import com.plattysoft.leonids.ParticleSystem;
 
 public class ConfettiExampleActivity extends Activity implements OnClickListener {
 	
@@ -23,9 +22,8 @@ public class ConfettiExampleActivity extends Activity implements OnClickListener
 		.setSpeedModuleAndAngleRange(0f, 0.1f, 180, 180)
 		.setRotationSpeed(144)
 		.setAcceleration(0.000017f, 90)
+		.setStartTime(15000)
 		.emit(findViewById(R.id.emiter_top_right), 8);
-
-		particleSystem.applyTime(15000);
 				
 		new ParticleSystem(this, 80, R.drawable.confeti3, 10000)
 		.setSpeedModuleAndAngleRange(0f, 0.1f, 0, 0)
